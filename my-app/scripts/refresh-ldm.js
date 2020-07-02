2bdecaabcbb3eaf8881c5c235ab62c3233309e10
Thao-Luong <thao.luong@gooddata.com>
2020-07-02 10:56:28 +0700

    get rebase

--- my-app/scripts/refresh-ldm.js
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env node
-
-const { workspace, backend } = require("../src/constants");
-const path = "./src/ldm/full.js";
-process.argv.push("--project-id", workspace, "--hostname", backend, "--output", path);
-require("../node_modules/.bin/gdc-catalog-export");
--- my-app/src/constants.js
+++
@@ -1,7 +0,0 @@
-// module.exports is important for setupProxy.js
-module.exports = {
-    appName: "my-app",
-    backend: "https://staging.anywhere.gooddata.com",
-    workspace: "workspace",
-    workspaceFilter: null, // regexp to filter workspaces in WorkspacePicker. Example: /test/ or /^GoodData/,
-};
